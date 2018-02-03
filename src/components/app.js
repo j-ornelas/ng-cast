8798d2267bd356cd1cfa1e03f36c2e077e0d6a8c
j-ornelas <johnnyoh1988@gmail.com>
2018-02-02 16:14:15 -0800

    Setup controller

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,16 @@
-angular.module('video-player')
-
-.component('app', {
+angular.module('video-player').component('app', {
   // TODO
+
+  
+
+  controller: function() {
+    this.videos = window.exampleVideoData;
+    this.currentVideo = window.exampleVideoData[0];
+    this.changeVideo = (video) => {
+      console.log('click');
+      this.currentVideo = video;
+    };
+  },
+
+  templateUrl: 'src/templates/app.html'
 });
