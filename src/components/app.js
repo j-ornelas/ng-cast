2d54f0f86d36688b2e531faaa3e4c205ef05319c
j-ornelas <johnnyoh1988@gmail.com>
2018-02-02 18:55:56 -0800

    setup callback function

--- src/components/app.js
+++ src/components/app.js
@@ -6,8 +6,13 @@ angular.module('video-player').component('app', {
   controller: function(youTube) {
     this.videos = window.exampleVideoData;
     this.currentVideo = window.exampleVideoData[0];
+    this.setState = (result) => {
+      this.videos = result.data.items;
+      this.currentVideo = result.data.items[0];
+    };
+    
     this.searchResults = (result) => {
-      youTube.getRepo(result);
+      youTube.getRepo(result, this.setState);
       
     };
     this.selectVideo = (video) => {
