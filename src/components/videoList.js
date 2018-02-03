415110d7ebb17287b5d4accea245e4c877325c1c
j-ornelas <johnnyoh1988@gmail.com>
2018-02-03 13:50:17 -0800

    added pagination methods

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -3,10 +3,25 @@ angular.module('video-player').component('videoList', {
 
   bindings: {
     onClick: '<',
-    videos: '<'
+    videos: '<',
+    searchResults: '<',
+    prevQuery: '<',
+    nextPageToken: '<',
+    pageStack: '<',
+    stackState: '<'
   },
   controller: function() {
-    
+    this.pageBack = () => {
+      console.log(this.pageStack);
+      this.pageStack.pop();
+      this.searchResults(this.prevQuery, this.pageStack[this.pageStack.length-1]);
+    };
+
+    this.pageForward = () => {
+      this.searchResults(this.prevQuery, this.nextPageToken);
+      this.pageStack.push(this.nextPageToken);
+      console.log(this.pageStack);
+    };
   },
   templateUrl: 'src/templates/videoList.html'
 });
