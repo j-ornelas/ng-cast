194e2a3f919ed0dc4058fe512b5d72b27f9fcea0
j-ornelas <johnnyoh1988@gmail.com>
2018-02-03 10:51:17 -0800

    Added autoplay function

--- src/components/search.js
+++ src/components/search.js
@@ -1,16 +1,22 @@
 angular.module('video-player').component('search', {
   // TODO
   bindings: {
-    searchResults: '<'
+    searchResults: '<',
+    autoPlayToggle: '<'
   },
   controller: function() {
     this.input = '',
+    this.cssObj = {'color': 'black'};
     this.clearResult = () => {
       this.result();
       this.input = '';
     },
     this.result = () => {
       this.searchResults(this.input);
+    };
+    this.autoPlayClick = () => {
+      this.autoPlayToggle();
+      this.cssObj.color = this.cssObj.color === 'red' ? 'black' : 'red';
     };
   
   },
