667d1ee353b9a4d89ddef822f29ea863ae36c6f2
j-ornelas <johnnyoh1988@gmail.com>
2018-02-02 17:35:05 -0800

    setup result method

--- src/components/search.js
+++ src/components/search.js
@@ -1,4 +1,13 @@
 angular.module('video-player').component('search', {
   // TODO
+  bindings: {
+    searchResults: '<'
+  },
+  controller: function() {
+    this.input = '',
+    this.result = () => {
+      this.searchResults(this.input);
+    };
+  },
   templateUrl: 'src/templates/search.html'
 });
