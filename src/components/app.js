e70ab41818c6eaaf75ef81a2e2a46688b52ae38f
j-ornelas <johnnyoh1988@gmail.com>
2018-02-02 20:01:17 -0800

    removed exampleVideoData

--- src/components/app.js
+++ src/components/app.js
@@ -4,22 +4,22 @@ angular.module('video-player').component('app', {
   
 
   controller: function(youTube) {
-    this.videos = window.exampleVideoData;
-    this.currentVideo = window.exampleVideoData[0];
+    this.videos = [];
+    this.currentVideo = {};
     this.setState = (result) => {
       this.videos = result.data.items;
       this.currentVideo = result.data.items[0];
     };
     
-    this.searchResults = (result) => {
-      youTube.getRepo(result, this.setState);
+    this.searchResults = (query) => {
+      youTube.getRepo(query, this.setState);
       
     };
     this.selectVideo = (video) => {
       console.log('click');
       this.currentVideo = video;
     };
-  
+    this.searchResults('keanu reeves sad');
   },
 
   templateUrl: 'src/templates/app.html'
