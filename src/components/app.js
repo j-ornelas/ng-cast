c7df01677aad9f71e3b1b8dbbb4011b06de1f0e9
j-ornelas <johnnyoh1988@gmail.com>
2018-02-02 17:34:18 -0800

    setup searchResults function

--- src/components/app.js
+++ src/components/app.js
@@ -3,13 +3,18 @@ angular.module('video-player').component('app', {
 
   
 
-  controller: function() {
+  controller: function(youTube) {
     this.videos = window.exampleVideoData;
     this.currentVideo = window.exampleVideoData[0];
-    this.changeVideo = (video) => {
+    this.searchResults = (result) => {
+      youTube.getRepo(result);
+      
+    };
+    this.selectVideo = (video) => {
       console.log('click');
       this.currentVideo = video;
     };
+  
   },
 
   templateUrl: 'src/templates/app.html'
