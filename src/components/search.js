4b60d98c23ceed586ae3edb280b3473790bfcf7c
j-ornelas <johnnyoh1988@gmail.com>
2018-02-02 20:01:56 -0800

    added clearSearch method

--- src/components/search.js
+++ src/components/search.js
@@ -1,14 +1,18 @@
 angular.module('video-player').component('search', {
   // TODO
   bindings: {
-    searchResults: '<',
-    setState: '<'
+    searchResults: '<'
   },
   controller: function() {
     this.input = '',
+    this.clearResult = () => {
+      this.result();
+      this.input = '';
+    },
     this.result = () => {
       this.searchResults(this.input);
     };
+  
   },
   templateUrl: 'src/templates/search.html'
 });
