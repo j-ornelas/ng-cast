ef5dad06dc01262d879fccc1d73129fac3b29190
j-ornelas <johnnyoh1988@gmail.com>
2018-02-03 10:50:33 -0800

    Set up Youtube autoplay button

--- src/components/app.js
+++ src/components/app.js
@@ -6,18 +6,22 @@ angular.module('video-player').component('app', {
   controller: function(youTube) {
     this.videos = [];
     this.currentVideo = {};
+    this.autoPlay = false;
     this.setState = (result) => {
       this.videos = result.data.items;
       this.currentVideo = result.data.items[0];
     };
     
     this.searchResults = (query) => {
-      youTube.getRepo(query, this.setState);
+      youTube.getRepo(query, this.setState, this.autoPlay);
       
     };
     this.selectVideo = (video) => {
       console.log('click');
       this.currentVideo = video;
+    };
+    this.autoPlayToggle = () => {
+      this.autoPlay = !this.autoPlay;
     };
     this.searchResults('keanu reeves sad');
   },
